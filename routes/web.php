e853ea4160d6e3c1b952e074a9618982e32d0261
sylsylviane <syl.sylviane1@gmail.com>
2026-02-13 16:34:03 -0500

    Add routes for users

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('login');
 });
+
+// Routes pour la gestion des usagers
+Route::get('/utilisateurs', [App\Http\Controllers\UserController::class, 'index'])->name('user.index');
+Route::get('/inscription', [App\Http\Controllers\UserController::class, 'create'])->name('user.create');
+Route::post('/inscription', [App\Http\Controllers\UserController::class, 'store'])->name('user.store');
+Route::get('/utilisateur/{user}', [App\Http\Controllers\UserController::class, 'show'])->name('user.show');
+Route::get('/modifier/utilisateur/{user}/', [App\Http\Controllers\UserController::class, 'edit'])->name('user.edit');
+
